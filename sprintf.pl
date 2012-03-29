875959d0adab2728f11d237e18370bc745b32489
Olivier Bilodeau <olivier@bottomlesspit.org>
2012-03-29 13:34:00 -0400

    a new sprintf test

--- sprintf.pl
+++ sprintf.pl
@@ -10,5 +10,8 @@ print sprintf("Passing empty array ref to a sprintf, warnings?\n", @$a_ref);
 
 print "Awesomessss!!!\n";
 
-my $a_ref = undef;
+print sprintf("Passing a variable but nothing consumes it, warning?\n", 'foo');
+
+$a_ref = undef;
 print sprintf("Passing undefined array ref to a sprintf, warnings?\n", @$a_ref);
+
