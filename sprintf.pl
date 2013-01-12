50e7e82303ae1e09b678367eb0e9b412d0d35612
Olivier Bilodeau <olivier@bottomlesspit.org>
2013-01-12 12:49:22 -0500

    updated perl sprintf stuff

--- sprintf.pl
+++ sprintf.pl
@@ -3,6 +3,9 @@
 use strict;
 use warnings;
 
+#
+# undef stuff
+#
 print sprintf("Passing undef to a sprintf, warnings?\n", undef);
 
 my $a_ref = [];
@@ -13,5 +16,10 @@ print "Awesomessss!!!\n";
 print sprintf("Passing a variable but nothing consumes it, warning?\n", 'foo');
 
 $a_ref = undef;
-print sprintf("Passing undefined array ref to a sprintf, warnings?\n", @$a_ref);
+#print sprintf("Passing undefined array ref to a sprintf, warnings?\n", @$a_ref);
 
+
+#
+# positional parameters
+#
+print sprintf("this is third %3\$s. this is first %1\$s.\n", "one", "two", "three");
