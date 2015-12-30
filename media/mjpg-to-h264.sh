51b735bb16ee7614dbd997eb3d3adbdca1c69b25
Olivier Bilodeau <olivier@bottomlesspit.org>
2015-12-30 14:25:46 -0500

    media: mjpg converter: whitespace fixes

--- media/mjpg-to-h264.sh
+++ media/mjpg-to-h264.sh
@@ -2,22 +2,22 @@
 #
 # Batch processes my camera's MJPG files into h264 ones to save some space
 #
-# Built upon my previous work: 
+# Built upon my previous work:
 # https://github.com/obilodeau/nautilus-scripts/blob/master/x264-encode.sh
 #
 # Copyright (c) 2011, 2012, 2015 Olivier Bilodeau <olivier@bottomlesspit.org>
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without modification, are
 # permitted provided that the following conditions are met:
-# 
+#
 #    1. Redistributions of source code must retain the above copyright notice, this list of
 #       conditions and the following disclaimer.
-# 
+#
 #    2. Redistributions in binary form must reproduce the above copyright notice, this list
 #       of conditions and the following disclaimer in the documentation and/or other materials
 #       provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY OLIVIER BILODEAU ``AS IS'' AND ANY EXPRESS OR IMPLIED
 # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 # FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL OLIVIER BILODEAU OR
@@ -27,7 +27,7 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # TODO: support automatic rotation of videos
 
 DIR=$1
@@ -45,7 +45,7 @@ X264_OPTS=
 #X264_OPTS=:interlaced
 
 # Audio codec
-# older Canon A70 required AUDIO_CODEC=copy because the weird raw PCM properties prevented 
+# older Canon A70 required AUDIO_CODEC=copy because the weird raw PCM properties prevented
 # transcoding into mp3lame
 AUDIO_CODEC=mp3lame
 
@@ -54,7 +54,7 @@ for FILE in `exiftool -r -if '$Compression =~ /MJPG/i' -filename $DIR  | grep -v
 	# first pass
 	echo "# File $FILE: First pass encoding"
 	cd /tmp
-	mencoder "$FILE" -ovc x264 -x264encopts pass=1:bitrate=$BITRATE$X264_OPTS -nosound -o /dev/null 
+	mencoder "$FILE" -ovc x264 -x264encopts pass=1:bitrate=$BITRATE$X264_OPTS -nosound -o /dev/null
 	if [ $? -ne  0 ]; then
 		echo "Something went wrong with file: $FILE, bailing out"
 		exit
