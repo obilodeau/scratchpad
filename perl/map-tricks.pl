569ea90b3258834fdee8441f443a3e83435e872e
Olivier Bilodeau <olivier@bottomlesspit.org>
2017-02-05 23:33:41 -0500

    perl: added URL query portion parse map one-liner

--- perl/map-tricks.pl
+++ perl/map-tricks.pl
@@ -15,3 +15,11 @@ say 'Trying to transform array keys by prepending with _';
 say 'Before: ' . Dumper($hashref);
 %$hashref = map { '_'.$_ => $hashref->{$_} } keys %$hashref;
 say 'After: ' . Dumper($hashref);
+
+
+say 'fill hash with split';
+my $query = "id=12345&foo=bar&bar=baz";
+say "Before: $query";
+my $parsed = {};
+%$parsed = map { split /=/ } split(/&/, $query);
+say 'After: ' . Dumper($parsed);
