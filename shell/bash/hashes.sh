b5e7da28adccb80e6beefa6b79c2809e23c736fd
Olivier Bilodeau <olivier@bottomlesspit.org>
2017-07-11 21:59:59 -0400

    bash: more associative array (hashes) examples

--- shell/bash/hashes.sh
+++ shell/bash/hashes.sh
@@ -5,3 +5,15 @@ declare -A A
 A=( ["B"]="X" ["C"]="Y" )
 
 echo "${A["B"]}"
+
+A["D"]=patate
+
+echo "${A[D]}"
+
+echo
+echo "Dumping all values"
+for i in "${!A[@]}"
+do
+  echo "key  : $i"
+  echo "value: ${A[$i]}"
+done
